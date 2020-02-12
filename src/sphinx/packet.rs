6106ddf68ae128180d73090873bc6505ff1a34fc
t-bast <bastuc@hotmail.fr>
2020-02-12 16:39:52 +0100

    Early experimentation with Curve25519

--- src/sphinx/packet.rs
+++ src/sphinx/packet.rs
@@ -30,3 +30,42 @@ pub fn create_packet() -> Packet {
     hmac: mac,
   }
 }
+
+#[cfg(test)]
+mod tests {
+  extern crate curve25519_dalek;
+
+  use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT as G;
+  use curve25519_dalek::ristretto::CompressedRistretto;
+  use curve25519_dalek::scalar::Scalar;
+  use rand::rngs::OsRng;
+
+  // TODO: zeroize secret stuff (see https://github.com/dalek-cryptography/x25519-dalek/blob/master/src/x25519.rs)
+
+  #[test]
+  fn test_generate_shared_secrets() {
+    let mut csprng = OsRng;
+    let k1 = Scalar::random(&mut csprng);
+    let k2 = Scalar::random(&mut csprng);
+    assert_ne!(k1, k2);
+
+    let e1 = k1 * G;
+    let e2 = k2 * G;
+    assert_ne!(e1, e2);
+    assert_eq!(k2 * e1, k1 * e2);
+
+    let c1 = e1.compress().to_bytes();
+    let c2 = e2.compress().to_bytes();
+    println!("{}", hex::encode(c1).as_str());
+    println!("{}", hex::encode(c2).as_str());
+    assert_ne!(c1, c2);
+
+    match CompressedRistretto::from_slice(&c1).decompress() {
+      None => {
+        println!("w00t? decompress failed...");
+        assert!(false);
+      },
+      Some(ee1) => assert_eq!(e1, ee1),
+    };
+  }
+}
