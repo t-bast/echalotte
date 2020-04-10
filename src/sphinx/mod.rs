0916d029a53545bafa62d991afd068e1ee5f637f
t-bast <bastuc@hotmail.fr>
2020-04-10 13:54:07 +0200

    Decrypt Sphinx packet
    Add fixed-size payload.
    It's easy to make the payloads variable-size, but it requires deciding on
    an encoding for payloads which is outside of the scope for now.

--- src/sphinx/mod.rs
+++ src/sphinx/mod.rs
@@ -9,10 +9,45 @@ mod stream;
 
 #[cfg(test)]
 mod tests {
-    // use super::packet::*;
+    use super::packet::*;
 
     #[test]
     fn encrypt_and_decrypt_packet() {
-        // TODO: here we should test packet creation/decryption (viewed as a consumer of the module)
+        let private_keys = vec![
+            "6bbadf5f42cdfc539596d591d2534615304cb650893e27665bb0d001c3a65b0b",
+            "5b9c310d55180fd1f7caa4205ee931cdddda385f64422ed007676fea98114f09",
+            "0b4fbcb5e042577f8facaddd362f5da99cf67747e42ab47a7aa3f108198fbe06",
+        ];
+        let public_keys = vec![
+            "749aa7ed60b246fe1060d4d60d4afc96a7839ad7a7b6050a3b4fc3ee5b2e154a",
+            "b039559cb7f499283fc42cecfd74640a17d53d5c1d355199838d3fa30616324e",
+            "5420c0cc481a2841a56d49f9c613eefa08d9dd0d6025e3ab86530f8487337443",
+        ];
+        let payloads = vec![
+            "01010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101",
+            "02020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202",
+            "03030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303",
+        ];
+        let hops: Vec<Hop> = (0..3)
+            .map(|i| {
+                let mut hop = Hop {
+                    pubkey: [0u8; 32],
+                    payload: [0u8; HOP_PAYLOAD_SIZE],
+                };
+                hop.pubkey
+                    .copy_from_slice(hex::decode(public_keys[i]).unwrap().as_slice());
+                hop.payload
+                    .copy_from_slice(hex::decode(payloads[i]).unwrap().as_slice());
+                hop
+            })
+            .collect();
+        let mut p = Packet::new(&hops);
+        assert_eq!(p.version, 0);
+        for i in 0..3 {
+            let mut private_key = [0u8; 32];
+            private_key.copy_from_slice(hex::decode(private_keys[i]).unwrap().as_slice());
+            let hop_payload = p.decrypt(&private_key);
+            assert_eq!(hop_payload.to_vec(), hops[i].payload.to_vec());
+        }
     }
 }
--- src/sphinx/packet.rs
+++ src/sphinx/packet.rs
@@ -19,7 +19,13 @@ use super::stream;
 // TODO: zeroize session_key/eph_secrets/all secret data
 
 /// Sphinx payloads are constant-size to avoid leaking information.
-const PACKET_PAYLOAD_SIZE: usize = 1300;
+pub const PACKET_PAYLOAD_SIZE: usize = 1300;
+
+/// For simplicity, payloads are fixed-size.
+pub const HOP_PAYLOAD_SIZE: usize = 100;
+
+/// Payloads always contain a trailing mac to include for the next hop.
+const HOP_PAYLOAD_WITH_MAC_SIZE: usize = HOP_PAYLOAD_SIZE + mac::MAC_SIZE;
 
 /// A Sphinx packet. The payload is onion-encoded but doesn't leak any information about the number
 /// of hops used.
@@ -33,17 +39,17 @@ pub struct Packet {
 /// A hop in the onion route.
 pub struct Hop {
     pub pubkey: [u8; 32],
-    pub payload: Vec<u8>,
+    pub payload: [u8; HOP_PAYLOAD_SIZE],
 }
 
 impl Hop {
     pub fn len(&self) -> usize {
         // NB: a mac is always appended to payload data.
-        self.payload.len() + mac::MAC_SIZE
+        HOP_PAYLOAD_WITH_MAC_SIZE
     }
 
     pub fn is_empty(&self) -> bool {
-        self.payload.is_empty()
+        false
     }
 }
 
@@ -61,7 +67,26 @@ impl Packet {
     /// # Example
     ///
     /// ```
-    /// // TODO: provide a running example here.
+    /// extern crate curve25519_dalek;
+    /// extern crate rand;
+    ///
+    /// use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT as G;
+    /// use curve25519_dalek::scalar::Scalar;
+    /// use echalotte::sphinx::packet::{Hop, HOP_PAYLOAD_SIZE, Packet};
+    /// use rand::rngs::OsRng;
+    ///
+    /// let mut csprng = OsRng;
+    /// let hops = vec![
+    ///     Hop {
+    ///         pubkey: (Scalar::random(&mut csprng) * G).compress().to_bytes(),
+    ///         payload: [1u8; HOP_PAYLOAD_SIZE],
+    ///     },
+    ///     Hop {
+    ///         pubkey: (Scalar::random(&mut csprng) * G).compress().to_bytes(),
+    ///         payload: [2u8; HOP_PAYLOAD_SIZE],
+    ///     },
+    /// ];
+    /// let sphinx_packet = Packet::new(&hops);
     /// ```
     pub fn new(hops: &[Hop]) -> Packet {
         assert!(!hops.is_empty(), "you need to provide at least one hop");
@@ -74,18 +99,18 @@ impl Packet {
             hmac: [0u8; 32],
         };
         // Initialize payload with random bytes.
-        stream::generate_stream(&keys::generate_key(keys::KeyType::Stream, session_key.as_bytes()), &mut p.payload);
+        stream::generate_stream(
+            &keys::generate_key(keys::KeyType::Stream, session_key.as_bytes()),
+            &mut p.payload,
+        );
 
         let (shared_points, shared_secrets) = compute_shared_secrets(session_key, hops);
-        let filler = generate_filler(
-            &hops[0..hops.len() - 1],
-            &shared_secrets[0..hops.len() - 1],
-        );
+        let filler = generate_filler(&hops[0..hops.len() - 1], &shared_secrets[0..hops.len() - 1]);
         for i in (0..hops.len()).rev() {
             // Apply filler only to the recipient's payload.
             let filler_opt: Option<&[u8]> = match i {
                 i if i == hops.len() - 1 => Some(&filler),
-                _ => None
+                _ => None,
             };
             p.encrypt(&hops[i], &shared_secrets[i], shared_points[i], filler_opt);
         }
@@ -93,13 +118,19 @@ impl Packet {
     }
 
     /// Add one layer of onion encryption.
-    fn encrypt(&mut self, hop: &Hop, shared_secret: &SharedSecret, shared_point: RistrettoPoint, filler: Option<&[u8]>) {
+    fn encrypt(
+        &mut self,
+        hop: &Hop,
+        shared_secret: &SharedSecret,
+        shared_point: RistrettoPoint,
+        filler: Option<&[u8]>,
+    ) {
         // Insert hop payload and previous mac.
         let shift = hop.len();
         for i in (shift..PACKET_PAYLOAD_SIZE).rev() {
             self.payload[i] = self.payload[i - shift];
         }
-        self.payload[0..hop.payload.len()].copy_from_slice(hop.payload.as_slice());
+        self.payload[0..hop.payload.len()].copy_from_slice(&hop.payload);
         self.payload[hop.payload.len()..shift].copy_from_slice(&self.hmac);
 
         // Encrypt.
@@ -118,6 +149,49 @@ impl Packet {
         self.hmac = mac::compute(&mu, &self.payload);
         self.pubkey = shared_point.compress().to_bytes();
     }
+
+    pub fn decrypt(&mut self, key: &[u8; 32]) -> [u8; HOP_PAYLOAD_SIZE] {
+        // Derive shared secret.
+        // TODO: better handling of invalid public keys.
+        let eph_pubkey = CompressedRistretto::from_slice(&self.pubkey)
+            .decompress()
+            .expect("invalid hop public key provided");
+        let shared_secret = SharedSecret(hash::compute(
+            &(Scalar::from_bytes_mod_order(*key) * eph_pubkey).compress().to_bytes(),
+        ));
+        let blinding_factor = {
+            let to_hash = [eph_pubkey.compress().to_bytes(), shared_secret.clone().0].concat();
+            Scalar::from_bytes_mod_order(hash::compute(to_hash.as_slice()))
+        };
+        self.pubkey = (blinding_factor * eph_pubkey).compress().to_bytes();
+
+        // Verify mac.
+        let mu = keys::generate_key(keys::KeyType::Mac, &shared_secret.0);
+        assert_eq!(self.hmac, mac::compute(&mu, &self.payload), "invalid mac");
+
+        // Decrypt.
+        let rho = keys::generate_key(keys::KeyType::Stream, &shared_secret.0);
+        let mut stream = [0u8; PACKET_PAYLOAD_SIZE + HOP_PAYLOAD_WITH_MAC_SIZE];
+        stream::generate_stream(&rho, &mut stream);
+        self.payload
+            .iter_mut()
+            .zip(stream[0..PACKET_PAYLOAD_SIZE].iter())
+            .for_each(|(x, y)| *x ^= *y);
+        let mut hop_payload = [0u8; HOP_PAYLOAD_SIZE];
+        hop_payload.copy_from_slice(&self.payload[0..HOP_PAYLOAD_SIZE]);
+        self.hmac
+            .copy_from_slice(&self.payload[HOP_PAYLOAD_SIZE..HOP_PAYLOAD_WITH_MAC_SIZE]);
+
+        // Prepare next packet.
+        for i in 0..PACKET_PAYLOAD_SIZE - HOP_PAYLOAD_WITH_MAC_SIZE {
+            self.payload[i] = self.payload[i + HOP_PAYLOAD_WITH_MAC_SIZE];
+        }
+        self.payload[PACKET_PAYLOAD_SIZE - HOP_PAYLOAD_WITH_MAC_SIZE..PACKET_PAYLOAD_SIZE]
+            .copy_from_slice(&stream[PACKET_PAYLOAD_SIZE..PACKET_PAYLOAD_SIZE + HOP_PAYLOAD_WITH_MAC_SIZE]);
+
+        // Return decrypted payload.
+        hop_payload
+    }
 }
 
 fn compute_shared_secrets(session_key: Scalar, hops: &[Hop]) -> (Vec<RistrettoPoint>, Vec<SharedSecret>) {
@@ -128,7 +202,9 @@ fn compute_shared_secrets(session_key: Scalar, hops: &[Hop]) -> (Vec<RistrettoPo
     for hop in hops {
         eph_secret = blinding_factor * eph_secret;
         // TODO: better handling of invalid public keys.
-        let hop_pubkey: RistrettoPoint = CompressedRistretto::from_slice(&hop.pubkey).decompress().expect("invalid hop public key provided");
+        let hop_pubkey: RistrettoPoint = CompressedRistretto::from_slice(&hop.pubkey)
+            .decompress()
+            .expect("invalid hop public key provided");
         let eph_pubkey = eph_secret * G;
         let shared_secret = SharedSecret(hash::compute(&(eph_secret * hop_pubkey).compress().to_bytes()));
         shared_secrets.push(shared_secret.clone());
@@ -142,7 +218,11 @@ fn compute_shared_secrets(session_key: Scalar, hops: &[Hop]) -> (Vec<RistrettoPo
 }
 
 fn generate_filler(payloads: &[Hop], secrets: &[SharedSecret]) -> Vec<u8> {
-    assert_eq!(payloads.len(), secrets.len(), "the number of payloads doesn't match the number of secrets");
+    assert_eq!(
+        payloads.len(),
+        secrets.len(),
+        "the number of payloads doesn't match the number of secrets"
+    );
     let mut filler: Vec<u8> = Vec::new();
     let payloads_and_secrets: Vec<(&Hop, &SharedSecret)> = payloads.iter().zip(secrets.iter()).collect();
     for (p, secret) in payloads_and_secrets {
@@ -151,7 +231,10 @@ fn generate_filler(payloads: &[Hop], secrets: &[SharedSecret]) -> Vec<u8> {
         stream::generate_stream(&rho, &mut stream);
         filler.append(&mut vec![0u8; p.len()]);
         let to_skip = stream.len() - filler.len();
-        filler.iter_mut().zip(stream.iter().skip(to_skip)).for_each(|(x, y)| *x ^= *y);
+        filler
+            .iter_mut()
+            .zip(stream.iter().skip(to_skip))
+            .for_each(|(x, y)| *x ^= *y);
     }
     filler
 }
@@ -168,9 +251,10 @@ mod tests {
     fn create_hop(pubkey: &str, payload: &str) -> Hop {
         let mut hop = Hop {
             pubkey: [0u8; 32],
-            payload: hex::decode(payload).unwrap(),
+            payload: [0u8; HOP_PAYLOAD_SIZE],
         };
         hop.pubkey.copy_from_slice(hex::decode(pubkey).unwrap().as_slice());
+        hop.payload.copy_from_slice(hex::decode(payload).unwrap().as_slice());
         hop
     }
 
@@ -194,7 +278,7 @@ mod tests {
 
         match CompressedRistretto::from_slice(&c1).decompress() {
             Some(ee1) => assert_eq!(e1, ee1),
-            None => assert!(false, "w00t? decompress failed..."),
+            None => panic!("w00t? decompress failed..."),
         };
     }
 
@@ -203,9 +287,18 @@ mod tests {
         let mut csprng = OsRng;
         let session_key = Scalar::random(&mut csprng);
         let hops = vec![
-            create_hop("16e6ca634d2a9fc9fa0b2f1a2d2f6b718b1a54af464e6e47d2d608419c9b773e", "010101010101010101010101010101"),
-            create_hop("58122677032dfbad9c8b51f5514f71ab1089c8d5a9bea2e39adc0a17c17b1e26", "0202020202020202020202020202020202020202"),
-            create_hop("f2548a3b8812a4eb175c255a2e20c850b32a5658abfce0d78652f32e3f48676d", "0303030303"),
+            create_hop(
+                "16e6ca634d2a9fc9fa0b2f1a2d2f6b718b1a54af464e6e47d2d608419c9b773e",
+                "01010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101",
+            ),
+            create_hop(
+                "58122677032dfbad9c8b51f5514f71ab1089c8d5a9bea2e39adc0a17c17b1e26",
+                "02020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202",
+            ),
+            create_hop(
+                "f2548a3b8812a4eb175c255a2e20c850b32a5658abfce0d78652f32e3f48676d",
+                "03030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303",
+            ),
         ];
         let (shared_points, shared_secrets) = compute_shared_secrets(session_key, &hops);
         let secrets: HashSet<[u8; 32]> = shared_secrets.iter().map(|ss| ss.0).collect();
@@ -218,9 +311,18 @@ mod tests {
     #[test]
     fn test_generate_filler() {
         let hops = vec![
-            create_hop("16e6ca634d2a9fc9fa0b2f1a2d2f6b718b1a54af464e6e47d2d608419c9b773e", "010101010101010101010101010101"),
-            create_hop("58122677032dfbad9c8b51f5514f71ab1089c8d5a9bea2e39adc0a17c17b1e26", "0202020202020202020202020202020202020202"),
-            create_hop("f2548a3b8812a4eb175c255a2e20c850b32a5658abfce0d78652f32e3f48676d", "0303030303"),
+            create_hop(
+                "16e6ca634d2a9fc9fa0b2f1a2d2f6b718b1a54af464e6e47d2d608419c9b773e",
+                "01010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101",
+            ),
+            create_hop(
+                "58122677032dfbad9c8b51f5514f71ab1089c8d5a9bea2e39adc0a17c17b1e26",
+                "02020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202",
+            ),
+            create_hop(
+                "f2548a3b8812a4eb175c255a2e20c850b32a5658abfce0d78652f32e3f48676d",
+                "03030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303",
+            ),
         ];
         let secrets = vec![
             SharedSecret([37u8; 32]),
@@ -228,7 +330,7 @@ mod tests {
             SharedSecret([112u8; 32]),
         ];
         let filler = generate_filler(&hops, &secrets);
-        assert_eq!(filler.len(), 136, "filler length should cover all payloads and macs");
+        assert_eq!(filler.len(), 396, "filler length should cover all payloads and macs");
     }
 
     #[test]
@@ -239,22 +341,12 @@ mod tests {
             payload: [0; PACKET_PAYLOAD_SIZE],
             hmac: [0; 32],
         };
-        let hop = create_hop("16e6ca634d2a9fc9fa0b2f1a2d2f6b718b1a54af464e6e47d2d608419c9b773e", "010101010101010101010101010101");
+        let hop = create_hop(
+            "16e6ca634d2a9fc9fa0b2f1a2d2f6b718b1a54af464e6e47d2d608419c9b773e",
+            "01010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101",
+        );
         p.encrypt(&hop, &SharedSecret([2u8; 32]), G, None);
         assert_eq!(p.pubkey, G.compress().to_bytes());
         assert_ne!(p.hmac, [0; 32]);
-    }
-
-    #[test]
-    fn test_create() {
-        let hops = vec![
-            create_hop("16e6ca634d2a9fc9fa0b2f1a2d2f6b718b1a54af464e6e47d2d608419c9b773e", "010101010101010101010101010101"),
-            create_hop("58122677032dfbad9c8b51f5514f71ab1089c8d5a9bea2e39adc0a17c17b1e26", "0202020202020202020202020202020202020202"),
-            create_hop("f2548a3b8812a4eb175c255a2e20c850b32a5658abfce0d78652f32e3f48676d", "0303030303"),
-        ];
-        let p = Packet::new(&hops);
-        assert_eq!(p.version, 0);
-        assert_ne!(p.hmac, vec![0u8; 32].as_slice());
-        assert_ne!(p.pubkey, vec![0u8; 32].as_slice());
     }
 }
